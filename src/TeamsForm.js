b4d075bb50273991bdd95176d7996b656f0f9dbb
lizburkechi <liz.burke.chi@gmail.com>
2021-09-20 12:36:28 -0500

    adjust forms

--- src/TeamsForm.js
+++ src/TeamsForm.js
@@ -2,12 +2,12 @@ import React from "react";
 
 class TeamsForm extends React.Component {
   state = {
-    team1: "",
-    team2: "",
+    team_1: "",
     player1: "",
     player2: "",
     player3: "",
     player4: "",
+    team_2: "",
     player5: "",
     player6: "",
     player7: "",
@@ -32,7 +32,6 @@ class TeamsForm extends React.Component {
         <h1 className="choose-teams-title">Choose Your
        Teams</h1>
        </div>
-        <br />
         <form className="team1-form">
           <h2 className="team-title">Team 1</h2>
           <input
@@ -41,40 +40,33 @@ class TeamsForm extends React.Component {
             value={this.state.team1}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player1"
             placeholder="player 1"
             value={this.state.player1}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player2"
             placeholder="player 2"
             value={this.state.player2}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player3"
             placeholder="player 3"
             value={this.state.player3}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player4"
             placeholder="player 4"
             value={this.state.player4}
             onChange={(e) => this.change(e)}
           />
-          <br />
-          <br />
           <button className="btn btn-info" onClick={(e) => this.onSubmit(e)}>Submit</button>
+          <br />
         </form>
-        <br />
-        <br />
         <form classname="team2-form">
           <h2 className="team-title">Team 2</h2>
           <input
@@ -83,28 +75,24 @@ class TeamsForm extends React.Component {
             value={this.state.team2}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player5"
             placeholder="player 1"
             value={this.state.player5}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player6"
             placeholder="player 2"
             value={this.state.player6}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player7"
             placeholder="player 3"
             value={this.state.player7}
             onChange={(e) => this.change(e)}
           />
-          <br />
           <input
             name="player8"
             placeholder="player 4"
@@ -112,14 +100,9 @@ class TeamsForm extends React.Component {
             onChange={(e) => this.change(e)}
           />
           <br />
-          <br />
           <button className="btn btn-info" onClick={(e) => this.onSubmit(e)}>Submit</button>
         </form>
-        <br />
-        <br />
         <div className="teams-footer">
-          <br />
-          <br />
           <a id="play-game-btn" className="btn btn-danger" href="/trivia">
             Let's Play!
           </a>
